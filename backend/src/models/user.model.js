a65b1e408a3213cd36ac0787965ec3c7276d68cf
Toma68 <79533980+toma68@users.noreply.github.com>
2022-11-24 10:01:24 +0100

    User sqlz model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,1 +1,39 @@
-//user model sequelize
+const {Sequelize} = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    login: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    role: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    nom : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    prenom : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email : {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},{
+    timestamps: false
+}
+);
+
+module.exports = User;
