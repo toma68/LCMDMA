4169051056f750a9600951abe00ffcacd7815ca7
Toma68 <79533980+toma68@users.noreply.github.com>
2022-12-02 09:31:42 +0100

    Voila l'avion :) ✈️✈️✈️

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/User.model');
+const User = require('../models/user.model');
 const TokenController = require('./token.controller');
 
 const bcrypt = require('bcrypt');
