82f5b2835e252568851110482146599f044144e3
jarod25 <63459117+jarod25@users.noreply.github.com>
2022-12-16 11:00:13 +0100

    Planning fini avec insertions à la main

--- frontend/src/components/planningpage/PlanningDimancheComponent.vue
+++ frontend/src/components/planningpage/PlanningDimancheComponent.vue
@@ -1,13 +1,5 @@
 <template>
   <v-container>
-    <br>
-    <br>
-    <br>
-    <br>
-    <div class="text-center">
-      <v-btn to="/planning/samedi" replace outlined class="mx-6">Samedi</v-btn>
-      <v-btn to="/planning/dimanche" replace outlined disabled class="mx-6">Dimanche</v-btn>
-    </div>
     <h1>Planning Dimanche</h1>
     <div class="timetable timetableDimanche"></div>
   </v-container>
@@ -19,9 +11,16 @@ export default {
   name: "PlanningDimancheComponent",
   mounted() {
     const timetableDimanche = new Timetable();
-    timetableDimanche.setScope(10, 20);
-    timetableDimanche.addLocations(["Scène 2"]);
+    timetableDimanche.setScope(10, 18);
+    timetableDimanche.addLocations(["Stand de tir", "Voyante", "Combat de joûtes", "Ecurie", "Menuisier Ebeniste", "Scène 2"]);
     timetableDimanche.addEvent('Concours de la plus belle armure', 'Scène 2', new Date(2023, 7, 9, 17), new Date(2023, 7, 9, 18));
+    timetableDimanche.addEvent('Création d une statue en bois', 'Menuisier Ebeniste', new Date(2023, 7, 9, 13), new Date(2023, 7, 9, 15));
+    timetableDimanche.addEvent('Tir à l arc', 'Stand de tir', new Date(2023, 7, 9, 14), new Date(2023, 7, 9, 14, 30));
+    timetableDimanche.addEvent('Tir à l arc', 'Stand de tir', new Date(2023, 7, 9, 12), new Date(2023, 7, 9, 12, 30));
+    timetableDimanche.addEvent('Voyance', 'Voyante', new Date(2023, 7, 9, 10), new Date(2023, 7, 9, 18));
+    timetableDimanche.addEvent('Balade en Poney / Cheval', 'Ecurie', new Date(2023, 7, 9, 11), new Date(2023, 7, 9, 11, 30));
+    timetableDimanche.addEvent('Balade en Poney / Cheval', 'Ecurie', new Date(2023, 7, 9, 16), new Date(2023, 7, 9, 16, 30));
+    timetableDimanche.addEvent('Combat de joûtes', 'Combat de joûtes', new Date(2023, 7, 9, 15), new Date(2023, 7, 9, 16));
     const renderer = new Timetable.Renderer(timetableDimanche);
     renderer.draw('.timetableDimanche'); // any css selector
   }
--- frontend/src/views/PlanningView.vue
+++ frontend/src/views/PlanningView.vue
@@ -5,8 +5,11 @@
       <br>
       <br>
       <br>
-      <PlanningSamediComponent></PlanningSamediComponent>
-      <PlanningDimancheComponent></PlanningDimancheComponent>
+      <div class="text-center">
+        <button v-on:click="showComponent = !showComponent" class="button">Changer de jour</button>
+      </div>
+      <PlanningSamediComponent v-if="showComponent"></PlanningSamediComponent>
+      <PlanningDimancheComponent v-else></PlanningDimancheComponent>
     </v-container>
   </main>
 </template>
@@ -17,5 +20,42 @@ import PlanningDimancheComponent from "@/components/planningpage/PlanningDimanch
 export default {
   name: 'PlanningView',
   components: { PlanningSamediComponent, PlanningDimancheComponent },
+  data() {
+    return {
+      showComponent: true
+    }
+  }
 }
 </script>
+
+<style scoped>
+.button{
+  height: 36px;
+  min-width: 64px;
+  padding: 0 16px;
+  font-size: .875rem;
+  border: thin solid;
+  align-items: center;
+  border-radius: 4px;
+  display: inline-flex;
+  flex: 0 0 auto;
+  font-weight: 500;
+  letter-spacing: .0892857143em;
+  justify-content: center;
+  outline: 0;
+  position: relative;
+  text-decoration: none;
+  text-indent: 0.0892857143em;
+  text-transform: uppercase;
+  transition-duration: .28s;
+  transition-property: box-shadow,transform,opacity;
+  transition-timing-function: cubic-bezier(.4,0,.2,1);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+
+</style>
