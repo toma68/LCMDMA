4c87a5aebd345a594a8dd7bf94128862cf30f867
RFridelance <fridelancer@gmail.com>
2023-01-05 11:48:07 +0100

    Doc Swagger, mise en place de l'authentification sur la page, pour les requètes le nécessitant. Pour l'instant createAchat et get livre d'or

--- backend/src/routers/livre-d-or.router.js
+++ backend/src/routers/livre-d-or.router.js
@@ -7,6 +7,23 @@ const router = Router();
 router.post("/",auth(1), LivreDOrController.createLivreDOrMessage);
 
 router.get("/",auth(3), LivreDOrController.getAllLivreDOrMessages);
+/**
+ * @swagger
+ * /api/livre-d-or/:
+ *   get:
+ *      security :
+ *          - Bearer: []
+ *      description: Permet de récupérer un livre d'or
+ *      tags:
+ *          - Livre d'or
+ *      responses:
+ *          '200':
+ *              description: Resource receive successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 router.get("/:id", LivreDOrController.getLivreDOrMessageById);
 
