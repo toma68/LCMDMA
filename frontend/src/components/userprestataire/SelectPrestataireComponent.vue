d28b45119255500056c3b5b4708fdfe4953c10c4
Toma68 <79533980+toma68@users.noreply.github.com>
2023-01-12 11:13:04 +0100

    fix

--- frontend/src/components/userprestataire/SelectPrestataireComponent.vue
+++ frontend/src/components/userprestataire/SelectPrestataireComponent.vue
@@ -3,31 +3,31 @@
     <br>
     <br>
     <br>
-    <h1 class="text-center">{{prestataire.nomEntreprise}}</h1>
+    <h1 className="text-center">{{ prestataire.nomEntreprise }}</h1>
     <br>
     <v-row>
-      <v-col class="col-6">
+      <v-col className="col-6">
         <v-img
             width="auto"
             :src="`${prestataire.image}`" alt="" class="rounded"></v-img>
         <br>
       </v-col>
-      <v-col class="col-6">
-         <v-card>
-           <v-card-title> {{prestataire.description}} </v-card-title>
-           <br>
-           <h3 class="mx-3">Le mot du prestataire : </h3>
-           <PrestataireComponent :prestataire="prestataire"/>
-           <br>
-           <h3 class="mx-3">Mes activités : </h3>
-           <div v-for="activites in prestataire.activites" :key="activites.id">
-           <v-card-title v-if="showInfos">{{ activites.nom }}</v-card-title>
-           <v-card-subtitle>{{ activites.description }}</v-card-subtitle>
-           <v-card-text v-if="showInfos">
-             <p>L'activité commence à {{ activites.heureDebut }} et se termine à {{ activites.heureFin }}</p>
-           </v-card-text>
-           </div>
-         </v-card>
+      <v-col className="col-6">
+        <v-card>
+          <v-card-title> {{ prestataire.description }}</v-card-title>
+          <br>
+          <h3 className="mx-3">Le mot du prestataire : </h3>
+          <PrestataireComponent :prestataire="prestataire"/>
+          <br>
+          <h3 className="mx-3">Mes activités : </h3>
+          <div v-for="activites in prestataire.activites" :key="activites.id">
+            <v-card-title v-if="showInfos">{{ activites.nom }}</v-card-title>
+            <v-card-subtitle>{{ activites.description }}</v-card-subtitle>
+            <v-card-text v-if="showInfos">
+              <p>L'activité commence à {{ activites.heureDebut }} et se termine à {{ activites.heureFin }}</p>
+            </v-card-text>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -36,6 +36,7 @@
 
 <script>
 import PrestataireComponent from "@/components/prestataire/PagePersonnalisee/PrestataireComponent.vue";
+
 export default {
   name: "SelectPrestataireComponent",
   components: {PrestataireComponent},
